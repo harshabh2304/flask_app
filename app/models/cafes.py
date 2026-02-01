8a67f3f1c329d073d65d6d2a378d884791f3f165
harshabh2304 <harshabh2304@gmail.com>
2026-02-01 13:15:04 +0530

    v4 final can deploy

--- app/models/cafes.py
+++ app/models/cafes.py
@@ -1,3 +1,4 @@
+from app import db
 class Cafe(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(300),nullable=False)
--- app/models/users.py
+++ app/models/users.py
@@ -1,3 +1,4 @@
+from app import db
 class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(100),nullable=False)
